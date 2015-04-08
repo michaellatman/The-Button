6245fbe6c7a0322eccbccd3d3246c682050d99e8
Michael Latman <michael@latman.org>
2015-04-08 13:41:06 -0400

    Initial Commit

---
+++ The Button/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  The Button
+//
+//  Created by Michael Latman on 4/8/15.
+//  Copyright (c) 2015 Michael Latman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ The Button/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  The Button
+//
+//  Created by Michael Latman on 4/8/15.
+//  Copyright (c) 2015 Michael Latman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
